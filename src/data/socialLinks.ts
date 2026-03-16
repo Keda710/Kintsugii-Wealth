beaf77946e2b7125ce8d68c1697037ccdfb515f8
Keval <keval7102004@gmail.com>
2026-03-16 16:27:50 +0530

    message about changes

--- src/data/socialLinks.ts
+++ src/data/socialLinks.ts
@@ -1,7 +1,7 @@
 import type { SocialLink } from '../types';
 
 export const SOCIAL_LINKS: SocialLink[] = [
-  { href: 'https://linkedin.com', icon: 'linkedin' },
-  { href: 'https://instagram.com', icon: 'instagram' },
-  { href: 'https://youtube.com', icon: 'youtube' },
+  { href: 'https://www.linkedin.com/company/kintsugii-wealth/', icon: 'linkedin' },
+  { href: 'https://www.instagram.com/kintsugiiwealth?utm_source=qr&igsh=dXE0ZXF5bnJ1bnA0', icon: 'instagram' },
+  { href: 'https://www.youtube.com/@KintsugiiWealth-d1i', icon: 'youtube' },
 ];
