fad88f9b900232160f1d8fb10cd07c97b833e561
Keval <keval7102004@gmail.com>
2026-04-13 14:24:44 +0530

    Initial commit

--- src/data/testimonials.ts
+++ src/data/testimonials.ts
@@ -6,13 +6,13 @@ export const TESTIMONIALS: Testimonial[] = [
       'Investing with Kintsugii Wealth have been the best decision I have made in recent years. Your guidance during bull and bear market have been invaluable for me. Also you have adopted to my conservative investment style and have still managed to design the portfolio which is giving me excellent returns and at the same time preparing well for my future.',
     author: 'Jaldhi Mehata',
     role: 'Associate Director , Accenture India',
-    image: 'public/C1.jpeg',
+    image: '/C1.jpeg',
   },
   {
     quote:
       'I’ve had the privilege of working with Priyank not just as a wealth manager, but also as a trusted friend. His guidance on investments and tax planning has been thoughtful, transparent, and consistently aligned with my long-term goals. What truly sets him apart is his ability to simplify complex financial decisions and provide advice you can genuinely rely on. Highly recommended for anyone looking for a dependable and insightful financial partner.',
     author: 'Renuka Pillai',
     role: 'Senior Manager, HCL Tech',
-    image: 'public/C2.jpeg',
+    image: '/C2.jpeg',
   },
 ];
